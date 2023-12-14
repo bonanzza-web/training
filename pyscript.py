fb1379d845d247ebfd11a702e52099600fdf6860
Dmitry Ananev <frukt07@inbox.ru>
2023-12-14 17:16:01 +0300

    fixed pyscript

--- pyscript.py
+++ pyscript.py
@@ -4,6 +4,6 @@ import random
 
 rannumber = random.randint(1, 5)
 
-films_list = ["Любовь и голуби", "Иван Васильевич", "Бриллиантовая рука", "12 стульев", "Москва слезам не верит")
+films_list = ["Любовь и голуби", "Иван Васильевич", "Бриллиантовая рука", "12 стульев", "Москва слезам не верит"]
 
-print(film_list[rannumber]))
+print(films_list[rannumber-1])
