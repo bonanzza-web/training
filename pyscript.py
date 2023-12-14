b9f2045668f3090ac6c8689e95e485e55800816f
Dmitry Ananev <frukt07@inbox.ru>
2023-12-14 17:06:15 +0300

    Create script

---
+++ pyscript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import random
+
+rannumber = random.randint(1, 5)
+
+films_list = ["Любовь и голуби", "Иван Васильевич", "Бриллиантовая рука", "12 стульев", "Москва слезам не верит")
+
+print(film_list[rannumber]))
