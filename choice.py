00e3d2192db5a6d61b3375f52dc26d249a86a77b
Dmitry Ananev <frukt07@inbox.ru>
2023-12-15 17:38:18 +0300

    new edit

--- choice.py
+++ choice.py
@@ -2,28 +2,29 @@
 
 import random
 
-def add_film(films, new_film)
+def add_film(films, new_film):
 	films.append(new_film)
 	print("New film added")
 
-def choice_film(films)
+def choice_film(films):
 	if not films:
 		print("Not added films")
 		return
 	random_film = random.choice(films)
 	print(random_film)
 
-def main()
+def main():
 	films = []
 	while True:
 		print("Print 1 for add a film")
 		print("Print 2 for choice a film")
 		print("Print 3 for exit")
 
-		choice = int(input("Choice an option")
+		choice = int(input("Choice an option"))
 
 		if choice == 1:
 			new_film = input("Enter film")
 			add_film(films, new_film)
 		elif choice == 2:
 			choice_film(films)
+			random_film(films)
